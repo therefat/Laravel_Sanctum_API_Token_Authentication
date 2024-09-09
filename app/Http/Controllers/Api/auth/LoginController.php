1dd11f7c6ebb8a23a8408bef0fd8d587f6952b1f
refathossen <refathossen2001@gmail.com>
2024-09-09 18:55:15 +0600

    login controller

--- app/Http/Controllers/Api/auth/LoginController.php
+++ app/Http/Controllers/Api/auth/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -10,8 +12,19 @@ class LoginController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(LoginRequest $request)
     {
         //
+        $user = User::where('email',$request->email)->first();
+        if(!$user || !\Hash::check($request->password, $user->password) ){
+            return response()->json([
+                'message'=>'The provided credentials are incorrect.'
+            ],401);
+        }
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ],200);
     }
 }
